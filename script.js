de226518d5627c68ed7b03e6060bb83f6f08d93d
Aaditya <aadityathapa977@gmail.com>
2024-09-06 22:01:03 +0545

    Reverting

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let darkBtn = document.querySelector(".btn-theme");
 let variables = document.querySelector(":root");
 function themeChanger() {
-   let currentTheme = getComputedStyle(variables)
-      .getPropertyValue("--theme")
-      .trim();
+   let currentTheme = getComputedStyle(variables).getPropertyValue("--theme").trim();
    console.log(currentTheme);
-   if (
-      currentTheme ==
-      "radial-gradient(343px at 46.3% 47.5%, rgb(242, 242, 242) 0%, rgb(241, 241, 241) 72.9%)"
-   ) {
+   if (currentTheme == "radial-gradient(343px at 46.3% 47.5%, rgb(242, 242, 242) 0%, rgb(241, 241, 241) 72.9%)") {
       variables.style.setProperty("--theme", "#121212");
       variables.style.setProperty("--text-color", "white");
    } else {
@@ -17,16 +12,5 @@ function themeChanger() {
          "radial-gradient(343px at 46.3% 47.5%, rgb(242, 242, 242) 0%, rgb(241, 241, 241) 72.9%)"
       );
       variables.style.setProperty("--text-color", "black");
-   }
-}
-function navBar() {
-   let nav = document.querySelector(".nav-items");
-   let isVisible = nav.classList.contains("nav-visible");
-   console.log(isVisible);
-   // Toggle class based on visibility
-   if (isVisible) {
-      nav.classList.toggle("nav-visible");
-   } else {
-      nav.classList.toggle("nav-visible");
    }
 }
